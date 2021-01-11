005bec2921966d8c8b13fc8d0f2fa550f5020833
chi <ma.duongchi@gmail.com>
2021-01-10 22:16:06 -0500

    Created Dialogue Scripts

---
+++ FabricPanic/Assets/Scripts/Chi/DialogueManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue<string> lines;
+    void Start()
+    {
+        lines = new Queue<string>();
+    }
+    public void BeginDialogue (Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+        lines.Clear();
+
+        foreach (string line in dialogue.lines)
+        {
+            lines.Enqueue(line);
+        }
+        DisplayNewLine();
+    }
+    public void DisplayNewLine()
+    {
+        if(lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string line = lines.Dequeue();
+        dialogueText.text = line;
+    }
+    void EndDialogue()
+    {
+        Debug.Log("");
+    }
+}
