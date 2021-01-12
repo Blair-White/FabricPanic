86201932e6045be25c7b3876d8a05426830036e6
m3hrara <mehrarasb@gmail.com>
2021-01-11 22:17:04 -0500

    Drag and Drop startup
    Very basic version of it so far. You can drag the "InventoryObject" around, or drop it on the MidDisplay for it to get placed on there.

---
+++ FabricPanic/Assets/Scripts/Mehrara/DropController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropController : MonoBehaviour
+{
+    public List<GameObject> inventoryList;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "InventoryObject")
+        {
+            collision.gameObject.GetComponent<DragController>().isColliding = true;
+
+           if(inventoryList.Count<=3) 
+           {
+                collision.gameObject.transform.position = new Vector3( 2 * (inventoryList.Count),transform.position.y, transform.position.z);
+                inventoryList.Add(collision.gameObject);
+                //Debug.Log(collision.gameObject.transform.position);
+           }
+        }
+    }
+}
