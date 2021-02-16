cfd91cef3458abf70842b60ca4ee152b0712ad65
m3hrara <mehrarasb@gmail.com>
2021-02-15 22:50:04 -0500

    Added sorting system

---
+++ FabricPanic/Assets/Scripts/Mehrara/FabricDrop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabricDrop : MonoBehaviour
+{
+    public int BasketType;
+    private List<GameObject> fabricList;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<FabricScript>().FabricType == BasketType)
+        {
+            collision.gameObject.GetComponent<DragController>().isColliding = true;
+            fabricList.Add(collision.gameObject);
+
+        }
+    }
+    void Update()
+    {
+        if (fabricList.Count > 0)
+        {
+            GameObject lastItem = fabricList[fabricList.Count - 1];
+            if (lastItem.GetComponent<DragController>().canBeDropped == true)
+            {
+                lastItem.SetActive(false);
+                lastItem.GetComponent<DragController>().isColliding = false;
+            }
+        }
+    }
+}
---
+++ FabricPanic/Assets/Scripts/Mehrara/FabricScript.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabricScript : MonoBehaviour
+{
+    public int FabricType;
+}
