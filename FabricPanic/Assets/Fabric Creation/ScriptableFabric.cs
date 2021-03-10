6c72c49e255e71815232ee1fa31e2eb7d40e9e32
Blair-White <blairjwhite@hotmail.com>
2021-03-10 14:06:49 -0500

    revamped fabric system

--- FabricPanic/Assets/Fabric Creation/ScriptableFabric.cs
+++ FabricPanic/Assets/Fabric Creation/ScriptableFabric.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-    public enum FabricMaterials { Cotton, Wool, Nylon, Cashmere}
-    public enum FabricAttribute { Sporty, Festive, Alluring }
+    public enum FabricColor1 { Red, Orange, Yellow, Green, Blue, Pink, Purple, Neutral, Multicolor}
+    public enum FabricColor2 { Red, Orange, Yellow, Green, Blue, Pink, Purple, Neutral, Multicolor}
+    public enum FabricPattern { Plain, Striped, PolkaDot, Plaid, Geometric, Floral, Graphic }
+    
+    public enum FabricTrait { Bright, Dark, Warm, Cool, Child, Feminine, Masculine, Sporty }
+    public enum FabricHolliday { Lunar, CherrBlossom, Christmas, Halloween}
     public enum FabricSeason { AllSeason, Winter, Spring, Summer, Fall }
 
 [CreateAssetMenu(fileName = "Fabric", menuName = "FabricCreator/Fabric", order = 1)]
 public class ScriptableFabric : ScriptableObject
 {    
     [SerializeField] public string FabricName; 
-    [SerializeField] public int PlayerLevelRequirement;
-    [SerializeField] public FabricMaterials[] FabricComposition;
-    [SerializeField] public FabricAttribute[] _FabricAttributes;
-    [SerializeField] public FabricSeason[] _FabricSeason;
+    [SerializeField] public FabricColor1 FabricColor1;
+    [SerializeField] public FabricColor2 FabricColor2;
+    [SerializeField] public FabricPattern FabricPattern;
+    [SerializeField] public FabricTrait FabricTraits;
+    [SerializeField] public FabricHolliday FabricHolliday;
+    [SerializeField] public FabricSeason _FabricSeason;
     [SerializeField] public Material AssetMaterial;
     [SerializeField] public Sprite UiAsset;   
 }
